9869e923c6274aca0401a173378ad5116a9cda83
zsdresd <60921377+zsdresd@users.noreply.github.com>
2021-01-30 15:28:43 +0800

    support SA

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 export PATH="$(cat PATH)"
 
-if [[ -n $RCLONE_CONFIG && -n $RCLONE_DESTINATION ]]; then
+if [[ -n $RCLONE_DESTINATION ]]; then
 	echo "Rclone config detected"
 	echo -e "$RCLONE_CONFIG" > rclone.conf
 	echo -e "$UNPACK_PASSWORDS" > /app/passwds.txt
@@ -12,6 +12,18 @@ if [[ -n $RCLONE_CONFIG && -n $RCLONE_DESTINATION ]]; then
 	chmod +x on-stop.sh
 	chmod +x unpack.sh
 fi
+
+wget --no-check-certificate -q $SA_ZIP_URL -O accounts.zip
+unzip -qq accounts.zip -d /app/
+rm -rf accounts.zip
+
+echo "
+[Google]
+type = drive
+scope = drive
+service_account_file = /app/accounts/$SA_INIT_FILE
+service_account_file_path = /app/accounts/
+" >> rclone.conf
 
 echo "rpc-secret=$ARIA2C_SECRET" >> aria2c.conf
 aria2c -q --conf-path=aria2c.conf&
